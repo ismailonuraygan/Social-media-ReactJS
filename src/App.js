8827d5c0c12866ec2700c51055882c7f6a4cf391
İsmail Onur AYGAN <72319363+ismailonuraygan@users.noreply.github.com>
2021-10-19 01:03:55 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Feed from './Feed';
 import LeftSideBar from './LeftSideBar';
+import RightSideBar from './RightSideBar';
 
 function App() {
   return (
+    
     <div className="App">
       <LeftSideBar />
       <Feed />
+      <RightSideBar/>
     </div>
+
   );
 }
 
--- src/RightSideBar.js
+++ src/RightSideBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './RightSideBar.css';
-import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from 'react-twitter-embed';
+import { TwitterTimelineEmbed, TwitterTweetEmbed } from 'react-twitter-embed';
 import SearchIcon from '@material-ui/icons/Search';
 
 
@@ -15,13 +15,11 @@ function RightSideBar() {
             <div className="rightSideBar__widgetContainer">
                 <h2>What's happening</h2>
 
-                <TwitterTweetEmbed tweetId={"1402320383932502021"} />
+                <TwitterTweetEmbed tweetId={"1422990222891966466"} />
                 <TwitterTimelineEmbed
                     sourceType="profile"
-                    screenName="WealthyEminence"
+                    screenName="reactjs"
                     options={{ height: 400 }} />
-                <TwitterShareButton url={"https://twitter.com/ayganismailonur"}
-                    options={{ text: "front-end web development", via: "10UR" }} />
             </div>
 
         </div>
